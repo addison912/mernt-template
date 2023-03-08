8f47f14ffabd7a350b18114b39874c1fa21fc861
Addison Moore <addisondrewmoore@gmail.com>
2023-03-08 11:28:33 -0800

    initial project config

--- packages/backend/ecosystem.config.js
+++ packages/backend/ecosystem.config.js
@@ -4,54 +4,54 @@
  *  production mode :: pm2 start ecosystem.config.js --only prod
  *  development mode :: pm2 start ecosystem.config.js --only dev
  */
- module.exports = {
-  apps: [
-    {
-      name: 'prod', // pm2 start App name
-      script: 'dist/server.js',
-      exec_mode: 'cluster', // 'cluster' or 'fork'
-      instance_var: 'INSTANCE_ID', // instance variable
-      instances: 2, // pm2 instance count
-      autorestart: true, // auto restart if process crash
-      watch: false, // files change automatic restart
-      ignore_watch: ['node_modules', 'logs'], // ignore files change
-      max_memory_restart: '1G', // restart if process use more than 1G memory
-      merge_logs: true, // if true, stdout and stderr will be merged and sent to pm2 log
-      output: './logs/access.log', // pm2 log file
-      error: './logs/error.log', // pm2 error log file
-      env: { // environment variable
-        PORT: 3000,
-        NODE_ENV: 'production',
-      },
+export const apps = [
+  {
+    name: 'prod',
+    script: 'dist/server.js',
+    exec_mode: 'cluster',
+    instance_var: 'INSTANCE_ID',
+    instances: 2,
+    autorestart: true,
+    watch: false,
+    ignore_watch: ['node_modules', 'logs'],
+    max_memory_restart: '1G',
+    merge_logs: true,
+    output: './logs/access.log',
+    error: './logs/error.log',
+    env: {
+      // environment variable
+      PORT: 3000,
+      NODE_ENV: 'production',
     },
-    {
-      name: 'dev', // pm2 start App name
-      script: 'ts-node', // ts-node
-      args: '-r tsconfig-paths/register --transpile-only src/server.ts', // ts-node args
-      exec_mode: 'cluster', // 'cluster' or 'fork'
-      instance_var: 'INSTANCE_ID', // instance variable
-      instances: 2, // pm2 instance count
-      autorestart: true, // auto restart if process crash
-      watch: false, // files change automatic restart
-      ignore_watch: ['node_modules', 'logs'], // ignore files change
-      max_memory_restart: '1G', // restart if process use more than 1G memory
-      merge_logs: true, // if true, stdout and stderr will be merged and sent to pm2 log
-      output: './logs/access.log', // pm2 log file
-      error: './logs/error.log', // pm2 error log file
-      env: { // environment variable
-        PORT: 3000,
-        NODE_ENV: 'development',
-      },
+  },
+  {
+    name: 'dev',
+    script: 'ts-node',
+    args: '-r tsconfig-paths/register --transpile-only src/server.ts',
+    exec_mode: 'cluster',
+    instance_var: 'INSTANCE_ID',
+    instances: 2,
+    autorestart: true,
+    watch: false,
+    ignore_watch: ['node_modules', 'logs'],
+    max_memory_restart: '1G',
+    merge_logs: true,
+    output: './logs/access.log',
+    error: './logs/error.log',
+    env: {
+      // environment variable
+      PORT: 3000,
+      NODE_ENV: 'development',
     },
-  ],
-  deploy: {
-    production: {
-      user: 'user',
-      host: '0.0.0.0',
-      ref: 'origin/master',
-      repo: 'git@github.com:repo.git',
-      path: 'dist/server.js',
-      'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js --only prod',
-    },
+  },
+];
+export const deploy = {
+  production: {
+    user: 'user',
+    host: '0.0.0.0',
+    ref: 'origin/master',
+    repo: 'git@github.com:repo.git',
+    path: 'dist/server.js',
+    'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js --only prod',
   },
 };
